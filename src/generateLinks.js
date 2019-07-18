1fca9f8fac7895a8692d9781d4f1824746385663
Allen <allencch@hotmail.com>
2019-07-18 13:56:45 +0800

    New Twitter class name

--- src/generateLinks.js
+++ src/generateLinks.js
@@ -7,7 +7,7 @@
   function createLink(src) {
     const elem = document.createElement('div');
     elem.setAttribute('class', 'my-link');
-    const largeSrc = src + ':large';
+    const largeSrc = src.replace(/name=[^&]+/, 'name=large');
     const name = getName(src);
     elem.innerHTML = `<a href="${largeSrc}">${name}</a>`;
     return elem;
@@ -23,9 +23,9 @@
   function generateLinks() {
     clearAll();
 
-    const imgs = document.querySelectorAll('.AdaptiveMedia-photoContainer img');
+    const imgs = document.querySelectorAll('.css-1dbjc4n.r-psjefw img');
     imgs.forEach(img => {
-      const closest = img.closest('.content');
+      const closest = img.closest('.css-1dbjc4n.r-1iusvr4.r-46vdb2.r-5f2r5o.r-bcqeeo');
       const elem = createLink(img.src);
       closest.appendChild(elem);
     });
