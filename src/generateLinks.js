e2a72c47f33212cdae5bd02413af8e400199568e
Allen <allencch@hotmail.com>
2019-11-13 14:35:42 +0800

    Update query

--- src/generateLinks.js
+++ src/generateLinks.js
@@ -26,7 +26,7 @@
     const imgs = document.querySelectorAll('.css-1dbjc4n.r-18u37iz.r-thb0q2 .r-psjefw .css-1dbjc4n img');
 
     imgs.forEach(img => {
-      const closest = img.closest('.css-1dbjc4n.r-1iusvr4.r-46vdb2.r-5f2r5o.r-bcqeeo');
+      const closest = img.closest('.css-1dbjc4n.r-1iusvr4');
       const elem = createLink(img.src);
       closest.appendChild(elem);
     });
